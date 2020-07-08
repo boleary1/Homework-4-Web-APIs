0afdf1c11a58cf1e324d116d0a637640f648ff36
Brian OLeary <oleary.brian1@gmail.com>
2020-07-07 21:20:00 -0400

    done for the night

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 var timeEl = document.querySelector(".time");
 var mainEl = document.getElementById("main");
 
-var secondsLeft = 10;
+var secondsLeft = 60;
 
 function setTime() {
   var timerInterval = setInterval(function() {
     secondsLeft--;
-    timeEl.textContent = secondsLeft + " seconds left till colorsplosion.";
+    timeEl.textContent = secondsLeft + " seconds left.";
 
     if(secondsLeft === 0) {
       clearInterval(timerInterval);
