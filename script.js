ce5dcfa71d9241b913a042dff86077e6b5aa1582
Brian OLeary <oleary.brian1@gmail.com>
2020-07-12 22:25:01 -0400

    java script

--- script.js
+++ script.js
@@ -1,29 +1,112 @@
-// var timeEl = document.querySelector(".time");
-// var mainEl = document.getElementById("main");
+const startButton = document.getElementById('start-btn')
+const nextButton = document.getElementById('next-btn')
+const questionContainerElement = document.getElementById('question-container')
 
-// var secondsLeft = 60;
+const questionsEl = document.getElementById('question')
+const answerButtonEl = document.getElementById('answer-buttons')
 
-// function setTime() {
-//   var timerInterval = setInterval(function() {
-//     secondsLeft--;
-//     timeEl.textContent = secondsLeft + " seconds left.";
+let shuffledQuestions, currentQuestionIndex
 
-//     if(secondsLeft === 0) {
-//       clearInterval(timerInterval);
-//       sendMessage();
-//     }
+startButton.addEventListener('click', startGame)
+nextButton.addEventListener('click', () =>{
+    currrentQuestionIndex++
+    setNextQuestion()
+})
 
-//   }, 1000);
-// }
+function startGame() {
+console.log('start game function working')
+startButton.classList.add('hide')
+shuffledQuestions = questions.sort(() => Math.random()-.5)
+currentQuestionIndex = 0
+questionContainerElement.classList.remove('hide')
+setNextQuestion()
+}
 
-// function sendMessage() {
-//   timeEl.textContent = " ";
+function setNextQuestion() {
+    resetState()
+    showQuestion(shuffledQuestions[currentQuestionIndex]);
 
-//   var imgEl = document.createElement("img");
+}
 
-//   imgEl.setAttribute("src", "images/image_1.jpg");
-//   mainEl.appendChild(imgEl);
+function resetState(){
+    clearStatusClass(document.body)
+    nextButton.classList.add('hide')
+    while (answerButtonEl.firstChild) {
+        answerButtonEl.removeChild (answerButtonEl.firstChild)
+    }
+}
+function showQuestion (question){
+    questionsEl.innerText = question.question
+    question.answers.forEach(answer => {
+        const button = document.createElement('button')
+        button.innerText = answer.text
+        button.classList.add('btn')
+        if (answer.correct){
+            button.dataset.correct = answer.correct
+        }
+        button.addEventListener('click', selectAnswer)
+        answerButtonEl.appendChild(button)
+    })
+}
 
-// }
+function selectAnswer(e) {
+    const selectedButton = e.target
+    const correct = selectedButton.dataset.correct
+    setStatusClass(document.body, correct)
+    Array.from(answerButtonEl.children).forEach(button => {
+        setStatusClass(button, button.dataset.correct)
+    })
+    if (shuffledQuestions.length > currentQuestionIndex + 1){
+    nextButton.classList.remove('hide')
+} else{
+    startButton.innerText ('restart')
+    startButton.classList.remove('hide')
+}
 
-// setTime();
+}
+
+function setStatusClass (element, correct) {
+    clearStatusClass(element)
+    if(correct) {
+        element.classList.add('correct')
+    }
+    else {
+        element.classList.add('wrong')
+    }
+}
+
+function clearStatusClass(element) {
+        element.classList.remove('correct')
+        element.classList.remove('wrong')
+}
+
+const questions = [
+    {
+        question: 'what is 2 + 2',
+        answers: [
+            {text: '4', correct: true },
+            {text: '22' , correct: false}
+        ]
+    },
+    {
+        question: 'what is 2 + 8',
+        answers: [
+            {text: '10', correct: true },
+            {text: '22' , correct: false}
+        ]
+    },
+    {
+        question: 'what is 2 + 20',
+        answers: [
+            {text: '22', correct: true },
+            {text: '6' , correct: false}
+        ]
+    },
+    {
+        question: 'what is 2 + 2',
+        answers: [
+            {text: '4', correct: true },
+            {text: '22' , correct: false}
+        ]
+    }
+]
