00898eeffe24a9f59806b0eac140d02c7690ea07
Brian OLeary <oleary.brian1@gmail.com>
2020-07-13 21:40:41 -0400

    score is working!

--- script.js
+++ script.js
@@ -1,54 +1,46 @@
 const startButton = document.getElementById('start-btn')
-// const nextButton = document.getElementById('next-btn')
 const questionContainerElement = document.getElementById('question-container')
 const questionsEl = document.getElementById('question')
 const answerButtonEl = document.getElementById('answer-buttons')
 
-let shuffledQuestions
 let currentQuestionIndex
-let score = 0
+let score = 0 ;
 
 
 startButton.addEventListener('click', startGame)
 
-// nextButton.addEventListener('click', () => {
-//     console.log ('nextButton clicked')
-//     currentQuestionIndex ++ ;
-//     setNextQuestion()
-// })
-
 function startGame() {
-console.log('start game function working')
-startButton.classList.add('hide')
-shuffledQuestions = questions.sort(() => Math.random()-.5)
-currentQuestionIndex = 0
-questionContainerElement.classList.remove('hide')
-setNextQuestion()
+    console.log('start game function working')
+    startButton.classList.add('hide')
+    currentQuestionIndex = 0
+    score = 0
+    questionContainerElement.classList.remove('hide')
+    setNextQuestion()
 }
 
 function setNextQuestion() {
     resetState()
-    showQuestion(shuffledQuestions[currentQuestionIndex]);
+    showQuestion(Questions[currentQuestionIndex]);
 
 }
 
-function resetState(){
+function resetState() {
     clearStatusClass(document.body)
-    // nextButton.classList.add('hide')
     while (answerButtonEl.firstChild) {
-        answerButtonEl.removeChild (answerButtonEl.firstChild)
+        answerButtonEl.removeChild(answerButtonEl.firstChild)
     }
 }
-function showQuestion (question){
+function showQuestion(question) {
     questionsEl.innerText = question.question
+
     question.answers.forEach(answer => {
         const button = document.createElement('button')
         button.innerText = answer.text
         button.classList.add('btn')
-        if (answer.correct){
+        if (answer.correct) {
+            score++
             button.dataset.correct = answer.correct
-            score ++
-            console.log ("score" , score)
+            console.log(score)
         }
         button.addEventListener('click', selectAnswer)
         answerButtonEl.appendChild(button)
@@ -56,29 +48,33 @@ function showQuestion (question){
 }
 
 function selectAnswer(e) {
+    document.getElementById('right-answers').innerHTML = score;
     const selectedButton = e.target
     const correct = selectedButton.dataset.correct
     setStatusClass(document.body, correct)
     Array.from(answerButtonEl.children).forEach(button => {
         setStatusClass(button, button.dataset.correct)
     })
-    if (shuffledQuestions.length > currentQuestionIndex + 1){
-    // nextButton.classList.remove('hide')
-    setTimeout(() => {      currentQuestionIndex ++ ;
-        setNextQuestion(); 
-    }, 750); //set time between questions
+    if (Questions.length > currentQuestionIndex + 1) {
+        setTimeout(() => {
+            currentQuestionIndex++;
+            setNextQuestion();
+        }, 750); //set time between questions
+
+    }
+    else {
+        setTimeout(() => {
+            startButton.innerText ='restart'
+            startButton.classList.remove('hide')
+            resetState()
+        }, 750); //set time between questions
+    }
 
 }
- else{
-    startButton.innerText ('restart')
-    startButton.classList.remove('hide')
-}
 
-}
-
-function setStatusClass (element, correct) {
+function setStatusClass(element, correct) {
     clearStatusClass(element)
-    if(correct) {
+    if (correct) {
         element.classList.add('correct')
     }
     else {
@@ -87,37 +83,37 @@ function setStatusClass (element, correct) {
 }
 
 function clearStatusClass(element) {
-        element.classList.remove('correct')
-        element.classList.remove('wrong')
+    element.classList.remove('correct')
+    element.classList.remove('wrong')
 }
 
-const questions = [
+const Questions = [
     {
         question: 'what is 2 + 2',
         answers: [
-            {text: '4', correct: true },
-            {text: '22' , correct: false}
+            { text: '4', correct: true },
+            { text: '22', correct: false }
         ]
     },
     {
         question: 'what is 2 + 8',
         answers: [
-            {text: '10', correct: true },
-            {text: '22' , correct: false}
+            { text: '10', correct: true },
+            { text: '22', correct: false }
         ]
     },
     {
         question: 'what is 2 + 20',
         answers: [
-            {text: '22', correct: true },
-            {text: '6' , correct: false}
+            { text: '22', correct: true },
+            { text: '6', correct: false }
         ]
     },
     {
         question: 'what is 2 + 2',
         answers: [
-            {text: '4', correct: true },
-            {text: '22' , correct: false}
+            { text: '4', correct: true },
+            { text: '22', correct: false }
         ]
     }
 ]
