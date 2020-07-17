4eabe5912090f519955238559ee31f58660e2924
Brian OLeary <oleary.brian1@gmail.com>
2020-07-16 22:02:29 -0400

    java script

--- script.js
+++ script.js
@@ -2,6 +2,8 @@ const startButton = document.getElementById('start-btn')
 const questionContainerElement = document.getElementById('question-container')
 const questionsEl = document.getElementById('question')
 const answerButtonEl = document.getElementById('answer-buttons')
+const rulesEl = document.getElementById('rules')
+const gameOverEl = document.getElementById('gameOver')
 const timeEl = document.querySelector(".time");
 let currentQuestionIndex;
 let score ;
@@ -19,6 +21,8 @@ function startGame() {
     secondsLeft = 10
     timer()
     questionContainerElement.classList.remove('hide')
+    rulesEl.classList.add('hide')
+    gameOverEl.classList.add('hide')
     setNextQuestion()
    
 }
@@ -35,17 +39,18 @@ function timer (){ //begin the timer
         timeEl.textContent = secondsLeft + " seconds left.";
         secondsLeft--;
 
-        if(secondsLeft === 0) {
+        if(secondsLeft === -1) { //moved from 0 to -1 do display would count down to 1
           clearInterval(timerInterval);
         timeEl.textContent = "times up!"
          }
          if (currentQuestionIndex == Questions.length - 1) {
              console.log('out of questions')
              clearInterval(timerInterval); //timer stops now
-             finalScore = score + secondsLeft + 2 //sums up score and time for a final score
+             finalScore = (score * 3 )+ secondsLeft + 2 //sums up score and time for a final score
              console.log("final score: ", finalScore );
 
          }
+
     
       }, 1000);
 }
@@ -81,17 +86,20 @@ function selectAnswer(e) {
     Array.from(answerButtonEl.children).forEach(button => {
         setStatusClass(button, button.dataset.correct)
     })
-    if (Questions.length > currentQuestionIndex + 1) {
+    if (Questions.length > currentQuestionIndex + 1) { //checks for more questions
         setTimeout(() => {
             currentQuestionIndex++;
             setNextQuestion();
-        }, 750); //set time between questions
+        }, 500); //set time between questions
 
     }
     else {
-        setTimeout(() => {
+        setTimeout(() => { //if the questions run out this happens
             startButton.innerText ='restart'
             startButton.classList.remove('hide')
+            questionContainerElement.classList.add('hide')
+            gameOverEl.classList.remove('hide')
+
             // console.log('I want the timer to stop now')
             // clearInterval(timerInterval); //added here to try to stop the timer when questions run out
             resetState()
@@ -99,11 +107,14 @@ function selectAnswer(e) {
         }, 750); //set time between questions
     }
     if (selectedButton.dataset = correct) {
-        ++ score; //tried moving ++ before to make it increment before displaying
+        score ++; //tried moving ++ before to make it increment before displaying
      }
      else{
-         timerInterval-5
+         secondsLeft -- //trying to subtract time for wrong answers
+         console.log('timecheck' , secondsLeft)
+
      }
+
      document.getElementById('right-answers').innerHTML = score; //moved to the bottom of the function, because it was not incrimenting correct
 
 }
